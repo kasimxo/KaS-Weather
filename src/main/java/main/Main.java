58e5397d4de6a04650b386d04841408625f0d67c
kasimxo <kasimxo@gmail.com>
2023-05-15 08:56:18 +0200

    Work on deleting municipio from DB

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -51,8 +51,6 @@ public class Main {
 		if(buffer!=null) {
 			mW.setScreen(buffer);
 		}
-		//mDB.showSchema("CODES");
-		//List<String> test = mDB.showTableContent("TEMPERATURA");
 		
 	}
 
--- src/main/java/view/Delete_Mun_Window.java
+++ src/main/java/view/Delete_Mun_Window.java
@@ -2,13 +2,17 @@ package view;
 
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
 
 public class Delete_Mun_Window extends JFrame {
 
 	private JPanel contentPane;
+	private JList list;
 
 	/**
 	 * Launch the application.
@@ -30,12 +34,22 @@ public class Delete_Mun_Window extends JFrame {
 	 * Create the frame.
 	 */
 	public Delete_Mun_Window() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Borrar municipio");
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lbl_Selection = new JLabel("Selecciona el municipio para borrar:");
+		lbl_Selection.setBounds(50, 63, 257, 15);
+		contentPane.add(lbl_Selection);
+		
+		DefaultListModel model = new DefaultListModel();
+		list = new JList();
+		list.setBounds(60, 90, 250, 50);
+		contentPane.add(list);
 	}
-
 }
